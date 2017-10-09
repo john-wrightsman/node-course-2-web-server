13c63c7085c665e3429a7771eeb3f833548e9b80
John.SurfacePro3 <John@SurfacePro3.sp3>
2017-10-09 15:27:08 -0400

    added projects page

--- server.js
+++ server.js
@@ -48,6 +48,13 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'Projects',
+		currentYear: new Date().getFullYear()
+	});
+});
+
 app.get('/bad', (req, res) => {
 	res.send({
 		errorMessage: 'Unable to handle request'
